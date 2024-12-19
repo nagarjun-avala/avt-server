43b5485dc7faead430a45fa21eecd80cf071dc6d
Nagarjun A <nagarjun.avala.official@gmail.com>
2024-12-19 20:45:30 +0530

    Created A Test Route With server Info as response

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const app = express()
+/* ROUTE IMPORTS */
+const indexRoute = require("./routes/indexRoute");
+
+/* CONFIGURATIONS */
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use("/api", indexRoute);
+
+const port = process.env.PORT || 2000;
+app.listen(port, () => {
+  console.log({
+    port,
+    link: `http://localhost:${port}/api`,
+    testRoutes: [`http://localhost:${port}/api/test`],
+  });
+});
